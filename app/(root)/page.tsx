bf69f73b9f7d5ffbdd97b97accb397bfd7f4f76b
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-07-02 16:31:47 +0200

    Auth with clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function SetupPage() {
   return (
     <div className='p-4'>
-      <Button size='default' variant='default'>
-        Click me
-      </Button>
+      <UserButton afterSignOutUrl='/' />
     </div>
   );
 }
