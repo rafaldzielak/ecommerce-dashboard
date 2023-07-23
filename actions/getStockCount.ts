43bf0131317279b897aa33b29ba4af8dc30832a1
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-07-23 08:45:28 +0200

    [FE] Overview page - cards with shadcn

---
+++ actions/getStockCount.ts
@@ -0,0 +1,6 @@
+import prismadb from "@/lib/prismadb";
+
+export const getStockCount = async (storeId: string) => {
+  const salesCount = await prismadb.product.count({ where: { storeId: storeId, isArchived: false } });
+  return salesCount;
+};
---
+++ actions/getTotalRevenue.ts
@@ -0,0 +1,13 @@
+import prismadb from "@/lib/prismadb";
+
+export const getTotalRevenue = async (storeId: string) => {
+  const paidOrder = await prismadb.order.findMany({
+    where: { storeId: storeId, isPaid: true },
+    include: { orderItems: { include: { product: true } } },
+  });
+  const totalRevenue = paidOrder.reduce((total, order) => {
+    const orderTotal = order.orderItems.reduce((orderSum, item) => orderSum + item.product.price.toNumber(), 0);
+    return total + orderTotal;
+  }, 0);
+  return totalRevenue;
+};
